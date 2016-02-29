653c305aad99e076bce877dfceaf69d8090db780
Joseph Chingalo <profschingalo@gmail.com>
2016-02-29 17:26:15 +0300

    add some libray for tooltips

--- www/js/app.js
+++ www/js/app.js
@@ -461,5 +461,23 @@ angular.module('dataCapture', [
       return $sce.trustAsHtml(text);
     };
   }])
-
+  .directive('tooltip', function () {
+    return {
+      restrict: 'C',
+      link: function (scope, element, attrs) {
+        if (attrs.title) {
+          var $element = $(element);
+          $element.attr("title", attrs.title)
+          $element.tooltipster({
+            animation: attrs.animation,
+            trigger: "click",
+            position: "right",
+            positionTracker: true,
+            maxWidth: 500,
+            contentAsHTML: true
+          });
+        }
+      }
+    };
+  })
 ;
