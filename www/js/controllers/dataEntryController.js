3e0c801fd8e7bebadf38ea9ddf174f65d6bfdd09
Joseph Chingalo <profschingalo@gmail.com>
2016-04-04 18:11:14 +0300

    add loading on change pagination

--- www/js/controllers/dataEntryController.js
+++ www/js/controllers/dataEntryController.js
@@ -43,14 +43,18 @@ angular.module('dataCapture')
       $ionicLoading.hide();
     }
     $scope.navigateToNewPage = function(pageNumber,type){
+      $scope.data.loading = true;
       if(type == 'last'){
         if($localStorage.dataEntryData.formType == 'SECTION'){
           $scope.currentPage = $scope.data.selectedDataEntryForm.dataSet.sections.length - 1;
+          $scope.data.loading = false;
         }else{
           $scope.currentPage = parseInt($scope.data.selectedDataEntryForm.dataSet.dataElements.length/$scope.pageSizeDefault);
+          $scope.data.loading = false;
         }
       }else{
         $scope.currentPage = pageNumber;
+        $scope.data.loading = false;
       }
     };
 
