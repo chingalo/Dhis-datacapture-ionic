f3e3b14c87082c4d4a3798deca0f1dc762c14da6
Joseph Chingalo <profschingalo@gmail.com>
2016-04-05 14:52:32 +0300

    convert the heading from Camel notation to human readable sentence

--- www/js/controllers/aboutController.js
+++ www/js/controllers/aboutController.js
@@ -8,8 +8,12 @@ angular.module('dataCapture')
     $scope.data.systemInfo = $localStorage.systemInfo;
     $scope.data.appInfo = {
       Name : 'Dhis 2 Touch',
-      Version : '0.13',
-      'App revision' : '3e0c801'
+      Version : '0.14',
+      'App revision' : '3bfd452'
+    };
+
+    $scope.getSystemInfoName = function(key){
+      return (key.charAt(0).toUpperCase() + key.slice(1)).replace(/([A-Z])/g, ' $1').trim();
     }
 
   });
