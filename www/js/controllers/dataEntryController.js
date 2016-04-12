ee8e0dc481e59f2d0eff883ac0fe748a2f12ff13
Joseph Chingalo <profschingalo@gmail.com>
2016-04-12 11:17:52 +0300

    changeDataEntryForm and prepareToSave functions

--- www/js/controllers/dataEntryController.js
+++ www/js/controllers/dataEntryController.js
@@ -288,11 +288,14 @@ angular.module('dataCapture')
     //function to checking changes on data elements values from data entry form
     $scope.changeDataEntryForm = function(dataElement){
       for(var key in $scope.data.dataValues){
-        if($scope.data.dataValues[key]){
-          var value = $scope.data.dataValues[key];
-          prepareDataValuesToIndexDb(key,value,false);
-          if(dataElement.attributeValues.length > 0){
-            extendDataElementFunctions(dataElement,value);
+        var modelValue = key.split('-');
+        if(modelValue[0] == dataElement.id){
+          if($scope.data.dataValues[key]){
+            var value = $scope.data.dataValues[key];
+            prepareDataValuesToIndexDb(key,value,false);
+            if(dataElement.attributeValues.length > 0){
+              extendDataElementFunctions(dataElement,value);
+            }
           }
         }
       }
