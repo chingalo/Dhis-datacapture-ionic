1d3ff665d14842c2935e6248aab8343e3f6bd07e
Joseph Chingalo <profschingalo@gmail.com>
2016-04-20 18:03:12 +0300

    add data values status on local storage

--- www/js/controllers/aboutController.js
+++ www/js/controllers/aboutController.js
@@ -2,7 +2,7 @@
  * Created by chingalo on 3/16/16.
  */
 angular.module('dataCapture')
-  .controller('aboutController',function($scope,$localStorage){
+  .controller('aboutController',function($scope,$localStorage,sqlLiteServices){
 
     $scope.data = {};
     $scope.data.systemInfo = $localStorage.systemInfo;
@@ -12,6 +12,34 @@ angular.module('dataCapture')
       'App revision' : '446589e'
 
     };
+    $scope.data.appStorageStatus = {
+      synced : {value : 0,data : false},
+      unSynced : {value : 0,data : false}
+    };
+
+    getSyncedDataValues();
+    getUnSyncedDataValues();
+    function getSyncedDataValues(){
+      var value = 2;
+      sqlLiteServices.getAllDataByAttribute('dataValues', 'isSync',parseInt(value))
+        .then(function(dataValues){
+          $scope.data.appStorageStatus.synced = {
+            value : dataValues.length,
+            data :true
+          }
+        },function(){})
+    }
+    function getUnSyncedDataValues(){
+      var value = 1;
+      sqlLiteServices.getAllDataByAttribute('dataValues', 'isSync',parseInt(value))
+        .then(function(dataValues){
+          $scope.data.appStorageStatus.unSynced = {
+            value : dataValues.length,
+            data :true
+          }
+        },function(){})
+    }
+
 
     $scope.getSystemInfoName = function(key){
       return (key.charAt(0).toUpperCase() + key.slice(1)).replace(/([A-Z])/g, ' $1').trim();
