5a50f6d833d5b6aa9838f897de2d7e35f9c0b2fe
Joseph Chingalo <profschingalo@gmail.com>
2016-04-21 16:19:58 +0300

    update app icon

--- www/js/controllers/aboutController.js
+++ www/js/controllers/aboutController.js
@@ -9,7 +9,7 @@ angular.module('dataCapture')
     $scope.data.appInfo = {
       Name : 'Dhis 2 Touch',
       Version : '0.18',
-      'App revision' : 'a6d5f7e9' 
+      'App revision' : 'a6d5f7e9'
 
     };
 
